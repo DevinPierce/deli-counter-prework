75c5180838099eb0b243bc386077f45a15c4ed2e
Devin Pierce <devinmpierce@gmail.com>
2018-05-10 00:52:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ def line(katz_deli)
   if (katz_deli.size == 0) 
     puts 'The line is currently empty.'
   else
-  announcement = "The line is currently:"
-  katz_deli.each_with_index do |name, i|
-    announcement += " #{i+1}. #{name}"
-  end
+    announcement = "The line is currently:"
+    katz_deli.each_with_index do |name, i|
+      announcement += " #{i+1}. #{name}"
+    end
   puts announcement
   end
 end
