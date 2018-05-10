c1e31f3c947de26ea8d1c2413a209ca002afb8dd
Devin Pierce <devinmpierce@gmail.com>
2018-05-10 00:49:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,7 @@ def line(katz_deli)
   if (katz_deli.size == 0) 
     puts 'The line is currently empty.'
   end
-  
+  announcement = "The line is currently:"
+  for name in katz_deli do
+    announcement += " "
 end
